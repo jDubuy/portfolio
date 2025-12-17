b8ce2ce1b8d3dcf0fb3b90e60cf7f56e335314bf
jules <jules.dubuy@agglo-larochelle.fr>
2025-12-17 12:06:56 +0100

    ajout d'une version anglais de mon portfolio

--- js/Projet/darkmode.js
+++ js/Projet/darkmode.js
@@ -6,14 +6,19 @@ document.addEventListener('DOMContentLoaded', () => {
   // Appliquer le thème stocké
   if (localStorage.getItem('theme') === 'dark') {
     body.classList.add('dark-mode');
-    icon.src = '../asset/png/soleil.png';
+    icon.src = icon.src.replace('lune.png', 'soleil.png');
     icon.alt = 'Icône mode clair';
   }
 
   toggle.addEventListener('click', () => {
     const isDark = body.classList.toggle('dark-mode');
     localStorage.setItem('theme', isDark ? 'dark' : 'light');
-    icon.src = isDark ? '../asset/png/soleil.png' : '../asset/png/lune.png';
+    
+    if (isDark) {
+      icon.src = icon.src.replace('lune.png', 'soleil.png');
+    } else {
+      icon.src = icon.src.replace('soleil.png', 'lune.png');
+    }
     icon.alt = isDark ? 'Icône mode clair' : 'Icône mode sombre';
   });
 });
